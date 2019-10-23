68a7944ac4163f082c2e35fdbf47f674f8b5afd3
lfc105u <icedrip7@gmail.com>
2019-10-24 00:41:17 +0800

    Update box.js

--- box.js
+++ box.js
@@ -16,7 +16,7 @@
     var th3=document.createElement('th')
     th3.innerHTML='最後上傳時間';
     var th4=document.createElement('th');
-    th4.innerHTML='最後上傳資料';
+    th4.innerHTML='目前安裝感測器種類';
     // 将列添加到行内
     tr.appendChild(th1);
     tr.appendChild(th2);
@@ -25,7 +25,7 @@
     // 将行添加到表格中
     tab.appendChild(tr);
     // 添加表格内容
-    for(var i=1;i<5;i++){
+    for(var i=1;i<2;i++){
         var tb_tr1=document.createElement('tr');
         var tr1_td1=document.createElement('td');
         tr1_td1.style.cssText="padding-right:50px;padding-left:50px;";
@@ -38,7 +38,7 @@
         tr1_td3.innerHTML='2019-10-21 21:13';
         var tr1_td4=document.createElement('td');
         tr1_td4.style.cssText="padding-right:50px;padding-left:50px;";
-        tr1_td4.innerHTML='溫度: 26℃';
+        tr1_td4.innerHTML='溫度、二氧化碳';
 
         //将列添加到行内
         tb_tr1.appendChild(tr1_td1);
@@ -48,5 +48,94 @@
         //将行添加到表格中
         tab.appendChild(tb_tr1);
     }
+    //
+    var tb_tr1=document.createElement('tr');
+        var tr1_td1=document.createElement('td');
+        tr1_td1.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td1.innerHTML='2';
+        var tr1_td2=document.createElement('td');
+        tr1_td2.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td2.innerHTML='嘉義(X:23.3124,Y:121.4347)';
+        var tr1_td3=document.createElement('td');
+        tr1_td3.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td3.innerHTML='2019-10-20 03:17';
+        var tr1_td4=document.createElement('td');
+        tr1_td4.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td4.innerHTML='溫度、影像';
+
+        //将列添加到行内
+        tb_tr1.appendChild(tr1_td1);
+        tb_tr1.appendChild(tr1_td2);
+        tb_tr1.appendChild(tr1_td3);
+        tb_tr1.appendChild(tr1_td4);
+        //将行添加到表格中
+        tab.appendChild(tb_tr1);
+        //
+    var tb_tr1=document.createElement('tr');
+        var tr1_td1=document.createElement('td');
+        tr1_td1.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td1.innerHTML='3';
+        var tr1_td2=document.createElement('td');
+        tr1_td2.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td2.innerHTML='嘉義(X:23.5876,Y:121.3814)';
+        var tr1_td3=document.createElement('td');
+        tr1_td3.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td3.innerHTML='2019-10-20 08:54';
+        var tr1_td4=document.createElement('td');
+        tr1_td4.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td4.innerHTML='二氧化碳、影像';
+
+        //将列添加到行内
+        tb_tr1.appendChild(tr1_td1);
+        tb_tr1.appendChild(tr1_td2);
+        tb_tr1.appendChild(tr1_td3);
+        tb_tr1.appendChild(tr1_td4);
+        //将行添加到表格中
+        tab.appendChild(tb_tr1);
+        //
+    var tb_tr1=document.createElement('tr');
+        var tr1_td1=document.createElement('td');
+        tr1_td1.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td1.innerHTML='4';
+        var tr1_td2=document.createElement('td');
+        tr1_td2.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td2.innerHTML='民雄(X:23.2145,Y:121.3432)';
+        var tr1_td3=document.createElement('td');
+        tr1_td3.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td3.innerHTML='2019-10-23 11:43';
+        var tr1_td4=document.createElement('td');
+        tr1_td4.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td4.innerHTML='溫度、二氧化碳';
+
+        //将列添加到行内
+        tb_tr1.appendChild(tr1_td1);
+        tb_tr1.appendChild(tr1_td2);
+        tb_tr1.appendChild(tr1_td3);
+        tb_tr1.appendChild(tr1_td4);
+        //将行添加到表格中
+        tab.appendChild(tb_tr1);
+        //
+    var tb_tr1=document.createElement('tr');
+        var tr1_td1=document.createElement('td');
+        tr1_td1.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td1.innerHTML='5';
+        var tr1_td2=document.createElement('td');
+        tr1_td2.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td2.innerHTML='民雄(X:23.4632,Y:121.7464)';
+        var tr1_td3=document.createElement('td');
+        tr1_td3.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td3.innerHTML='2019-10-01 16:36';
+        var tr1_td4=document.createElement('td');
+        tr1_td4.style.cssText="padding-right:50px;padding-left:50px;";
+        tr1_td4.innerHTML='溫度';
+
+        //将列添加到行内
+        tb_tr1.appendChild(tr1_td1);
+        tb_tr1.appendChild(tr1_td2);
+        tb_tr1.appendChild(tr1_td3);
+        tb_tr1.appendChild(tr1_td4);
+        //将行添加到表格中
+        tab.appendChild(tb_tr1);
+        //
     // 将表格添加到页面中
     document.table.appendChild(tab);
